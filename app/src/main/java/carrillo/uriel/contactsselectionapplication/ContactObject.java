98e799030572b06facd567e32c4e9c7480f8db26
Uriel Carrillo <carrillouriel@gmail.com>
2015-11-29 14:05:45 -0700

    First Commit, Base Project

---
+++ app/src/main/java/carrillo/uriel/contactsselectionapplication/ContactObject.java
@@ -0,0 +1,52 @@
+package carrillo.uriel.contactsselectionapplication;
+
+import java.io.Serializable;
+
+/**
+ * Created by uriel on 29/11/15.
+ */
+public class ContactObject implements Serializable {
+    private static final long serialVersionUID=1L;
+    private String name;
+    private String phone;
+    private String website;
+
+    public ContactObject(String name, String phone, String website)
+    {
+        super();
+        this.name=name;
+        this.phone=phone;
+        this.website=website;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+
+    public String getPhone()
+    {
+        return  this.phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone=phone;
+    }
+
+    public String getWebsite()
+    {
+        return  this.website;
+    }
+
+    public void setWebsite(String website)
+    {
+        this.website=website;
+    }
+
+}
