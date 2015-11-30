39709eae39124ae34d941f84cc3d8ecaf442b3c6
Uriel Carrillo <uriel.carrillo@caffenio.com>
2015-11-30 10:09:38 -0700

    added methods to ContectIntentActivity and ContactPageActivity to read and show contact info, not functional yet

--- app/src/main/java/carrillo/uriel/contactsselectionapplication/ContactIntentActivity.java
+++ app/src/main/java/carrillo/uriel/contactsselectionapplication/ContactIntentActivity.java
@@ -1,5 +1,6 @@
 package carrillo.uriel.contactsselectionapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,59 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class ContactIntentActivity extends AppCompatActivity {
+import java.lang.Override;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactIntentActivity extends ActionBarActivity {
+
+    private final int PHONE=0;
+    private final int WEBSITE=1;
+    private ListView intentListView;
+    private ArrayAdapter<String> adapter;
+    private List<ContactObject> contactsList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Inflate your View
         setContentView(R.layout.activity_contact_intent);
+        intentListView = (ListView) findViewById(R.id.listView1);
+
+        //initialize the list and add item
+        contactsList = new ArrayList<ContactObject>();
+        contactsList.add(new ContactObject("Android One","111-1111-1111", "www.androidATC.com"));
+        contactsList.add(new ContactObject("Android Two","222-2222-2222", "www.androidATC.com"));
+        contactsList.add(new ContactObject("Android Three","333-3333-3333", "www.androidATC.com"));
+        contactsList.add(new ContactObject("Android Four","444-4444-4444", "www.androidATC.com"));
+
+        List<String> listName= new ArrayList<String>();
+        for(int i=0; i< contactsList.size();i++)
+        {
+            listName.add(contactsList.get(i).getName());
+        }
+
+        //initialize the ArrayAdapter object
+        adapter = new ArrayAdapter<String>(ContactIntentActivity.this,android.R.layout.simple_list_item_1,listName);
+        //Set the adapter of the ListView
+        intentListView.setAdapter(adapter);
+
+        intentListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Intent i = new Intent(ContactIntentActivity, this, ContactPageActivity.class);
+                i.putExtra("Object", contactsList.get(position));
+                startActivityForResult(i,0);
+
+            }
+
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -33,6 +80,27 @@ public class ContactIntentActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_contact_intent, menu);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(data==null){
+            return;
+        }
+        Bundle resultData=data.getExtras();
+        String value= resultData.getString("value");
+        switch(resultCode)
+        {
+            case PHONE:
+                //Implicit intent to make a call
+                startActivity(new Intent(Intent.ACTION_CALL,Uri.parse("tel:"+value)));
+                break;
+            case WEBSITE:
+                //implicit intent to visit website
+                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("http://"+value)));
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/carrillo/uriel/contactsselectionapplication/ContactPageActivity.java
+++ app/src/main/java/carrillo/uriel/contactsselectionapplication/ContactPageActivity.java
@@ -7,12 +7,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ContactPageActivity extends AppCompatActivity {
+public class ContactPageActivity extends ActionBarActivity implements OnClickListener {
+
+    private final int PHONE=0;
+    private final int WEBSITE=1;
+    private TextView contactName;
+    private TextView contactPhone;
+    private TextView contactWebsite;
+    private ContactObject contactObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_page);
+
+        contactName=(TextView) findViewById(R.id.contactName);
+        contactPhone=(TextView) findViewById(R.id.contactPhone);
+        contactWebsite=(TextView) findViewById(R.id.contactWebsite);
+
+        Bundle extras= getIntent().getExtras();
+        if(extras==null)
+            return;
+        contactObject=(ContactObject)getIntent().getSerializableExtra("Object");
+
+        contactName.setText(contactObject.getName());
+        contactPhone.setText("Phone:"+contactObject.getPhone());
+        contactWebsite.setText("Website:"+contactObject.getWebsite());
+
+        contactPhone.setOnClickListener(this);
+        contactWebsite.setOnClickListener(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +48,24 @@ public class ContactPageActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.contactPhone:
+                Intent i = new Intent();
+                i.putExtra("value",contactObject.getPhone());
+                setResult(PHONE,i);
+                finish();
+                break;
+            case R.id.contactWebsite:
+                i=new Intent();
+                i.putExtra("value",contactObject.getWebsite());
+                setResult(WEBSITE,i);
+                finish();
+                break;
+        }
     }
 
 }
